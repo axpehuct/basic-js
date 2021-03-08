9f4bdbfcb42cb1297bfa2014a7f5516f7c18af5d
Equento <axpehuct95@gmail.com>
2021-03-08 14:13:21 +0300

    feat: dream-team solved

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if(Array.isArray(members) === false) {
+    return false;
+  }
+  
+  let result = [];
+
+  for(let i = 0; i < members.length; ++i){
+    if(typeof members[i] === "string"){
+      let trimmedArr = members[i].trim();
+      let char = trimmedArr[0].toUpperCase();
+      result.push(char);
+    }
+  }
+  return  result.sort().join('');
 };
